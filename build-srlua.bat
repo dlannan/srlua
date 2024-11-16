c5ed252c2ea3ac7b2ce6b98c47939efcaae375e7
David Lannan <dlannan68@gmail.com>
2024-11-16 13:17:01 +1030

    updated include paths

--- build-srlua.bat
+++ build-srlua.bat
@@ -5,7 +5,7 @@ set LINK_FLAGS=/link
 set DEFS=/D_WIN32
 
 call cd LuaJIT-2.1\src
-call msvc.build.bat
+call msvcbuild.bat
 call cd ..
 call cl.exe %DEFS% /Fe:bin\windows\srlua.exe src\srlua.c %LINK_FLAGS%
 call cl.exe %DEFS% /Fe:bin\windows\glue.exe src\glue.c %LINK_FLAGS%
